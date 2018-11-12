fdde144cfce9e42f48c8ea9b93ee86907db0ee7e
Ryan Mohta <ryanmohta@gmail.com>
2018-11-12 03:11:39 -0800

    Optimization

--- modechanger.js
+++ modechanger.js
@@ -2,17 +2,17 @@ var date = new Date();
 var hours = date.getHours();
 var cssLinkIndex = 2;
 if(hours < 7 || hours > 17) {
-  //var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
-  document.getElementById("modecss").href = "stylesheets/darkmode.css";
+  document.getElementsByTagName("link")[cssLinkIndex].href = "stylesheets/darkmode.css";
 }
 else {
-  var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
+  document.getElementsByTagName("link")[cssLinkIndex].href = "stylesheets/lightmode.css";
+  /*var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
 
     var newlink = document.createElement("link");
     newlink.setAttribute("rel", "stylesheet");
     newlink.setAttribute("type", "text/css");
     newlink.setAttribute("href", "stylesheets/lightmode.css");
 
-    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);*/
 
 }
