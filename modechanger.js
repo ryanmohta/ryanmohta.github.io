759d860a3cbe799b734e08c70ea56f56b1d16428
Ryan Mohta <ryanmohta@gmail.com>
2019-01-12 23:46:10 -0800

    updated bugz

--- modechanger.js
+++ modechanger.js
@@ -5,6 +5,7 @@ var timerVariable = setInterval(myTimer, 1000);
 function myTimer() {
   var date = new Date();
   var hour = date.getHours();
+  var minute = date.getMinutes();
   var cssLinkIndex = 2;
   if(hour < 7 || (hour > 16 && minute > 29) || hour > 17) {
     document.getElementsByTagName("link")[cssLinkIndex].href = "stylesheets/darkmode.css";
