c2055efb1bc9aa106e54d23b289179aec49bb513
Ryan Mohta <ryanmohta@gmail.com>
2019-01-12 23:37:17 -0800

    Updated timer

--- modechanger.js
+++ modechanger.js
@@ -1,9 +1,15 @@
-var date = new Date();
-var hours = date.getHours();
-var cssLinkIndex = 2;
-if(hours < 7 || hours > 17) {
-  document.getElementsByTagName("link")[cssLinkIndex].href = "stylesheets/darkmode.css";
-}
-else {
-  document.getElementsByTagName("link")[cssLinkIndex].href = "stylesheets/lightmode.css";
+document.addEventListener("DOMContentLoaded", myTimer);
+
+var timerVariable = setInterval(myTimer, 1000);
+
+function myTimer() {
+  var date = new Date();
+  var hours = date.getHours();
+  var cssLinkIndex = 2;
+  if(hours < 7 || hours > 17) {
+    document.getElementsByTagName("link")[cssLinkIndex].href = "stylesheets/darkmode.css";
+  }
+  else {
+    document.getElementsByTagName("link")[cssLinkIndex].href = "stylesheets/lightmode.css";
+  }
 }
